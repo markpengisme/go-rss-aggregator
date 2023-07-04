0cfadbadd24dd30bd1a48718b976cf080e13688e
MarkPeng <markpengisme@gmail.com>
2023-07-04 23:47:25 +0800

    API Key & Feeds

--- main.go
+++ main.go
@@ -56,6 +56,9 @@ func main() {
 	v1Router.Get("/health", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 	v1Router.Post("/users", apiCfg.handlerUsersCreate)
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerUsersGet))
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerFeedCreate))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
